a86c05815b41ef84410647c44284a01a39cb5d6f
Floriberto Hernandez Jr <fhjr10code@zoho.com>
2022-02-08 15:34:15 -0800

    Navbar component added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// Tools
+import React from 'react';
+
+// Components
+import Navbar from './components/layout/Navbar';
+
+// Styles
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Navbar title="Github Finder" icon="fa-brands fa-github"/>
     </div>
   );
 }
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Navbar({ title, icon }) {
+  return (
+    <nav className='bg-orange-400 border-gray-200 px-2 sm:px-4 py-2.5 text-xl'>
+      <i className={icon}></i> {title}
+    </nav>
+  );
+}
+
+Navbar.defaultProps = {
+  title: 'Github Finder Tut',
+  icon: 'fa-brands fa-github',
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
+
+export default Navbar;
