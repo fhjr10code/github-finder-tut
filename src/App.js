ac3c566b24860fce19e2a1cb48398fd0b4370639
Floriberto Hernandez Jr <fhjr10code@zoho.com>
2022-03-08 17:29:50 -0800

    added an about page and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 // Tools
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import axios from 'axios';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Pages
+import About from './components/pages/About';
 
 // Components
 import Navbar from './components/layout/Navbar';
@@ -12,52 +16,64 @@ import Alert from './components/layout/Alert';
 import './index.css';
 
 const App = () => {
-	const [users, setUsers] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const [alerts, setAlerts] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [alerts, setAlerts] = useState(null);
 
-	// Search for users in github and place them in state
-	const searchUsers = async text => {
-		setLoading(true);
+  // Search for users in github and place them in state
+  const searchUsers = async (text) => {
+    setLoading(true);
 
-		const res = await axios.get(
-			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-		);
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
-		setUsers(res.data.items);
-		setLoading(false);
-	};
+    setUsers(res.data.items);
+    setLoading(false);
+  };
 
-	// Clear users from state
-	const clearUsers = () => {
-		setUsers([]);
-		setLoading(false);
-	};
+  // Clear users from state
+  const clearUsers = () => {
+    setUsers([]);
+    setLoading(false);
+  };
 
-	// Alert user of an error
-	const setAlert = (msg, type) => {
-		setAlerts(alerts => {
-			return { ...alerts, msg: msg, type: type };
-		});
+  // Alert user of an error
+  const setAlert = (msg, type) => {
+    setAlerts((alerts) => {
+      return { ...alerts, msg: msg, type: type };
+    });
 
-		setTimeout(() => setAlerts(null), 3000);
-	};
+    setTimeout(() => setAlerts(null), 3000);
+  };
 
-	return (
-		<div>
-			<Navbar title='Github Finder' icon='fa-brands fa-github' />
-			<div>
-				<Alert alerts={alerts} />
-				<Search
-					searchUsers={searchUsers}
-					clearUsers={clearUsers}
-					showClear={users.length > 0 ? true : false}
-					setAlert={setAlert}
-				/>
-				<Users loading={loading} users={users} />
-			</div>
-		</div>
-	);
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar title='Github Finder' icon='fa-brands fa-github' />
+        <div>
+          <Alert alerts={alerts} />
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <Fragment>
+                  <Search
+                    searchUsers={searchUsers}
+                    clearUsers={clearUsers}
+                    showClear={users.length > 0 ? true : false}
+                    setAlert={setAlert}
+                  />
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              }
+            />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,14 +1,17 @@
 // Tools
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ title, icon }) => {
   return (
     <nav className='bg-orange-400 border-gray-200 px-2 sm:px-4 py-2.5 text-xl'>
       <i className={icon}></i> {title}
+      <Link to='/'> Home </Link>
+      <Link to='/about'> About </Link>
     </nav>
   );
-}
+};
 
 Navbar.defaultProps = {
   title: 'Github Finder Tut',
