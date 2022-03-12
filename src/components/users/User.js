672ca7188599c4a25b409eafc7d6f936127850b6
Floriberto Hernandez Jr <fhjr10code@zoho.com>
2022-03-12 07:44:04 -0800

    Customized User to show more info

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,6 +1,10 @@
 // Tools
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, Fragment } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+// Components
+import Spinner from '../layout/Spinner';
 
 const User = ({ getUser, user, loading }) => {
   const { login } = useParams();
@@ -10,6 +14,7 @@ const User = ({ getUser, user, loading }) => {
     location,
     bio,
     blog,
+    company,
     html_url,
     followers,
     following,
@@ -17,10 +22,70 @@ const User = ({ getUser, user, loading }) => {
     public_gists,
     hireable,
   } = user;
+
   useEffect(() => {
     getUser(login);
   }, []);
-  return <div>{name}</div>;
+
+  if (loading) return <Spinner />;
+  return (
+    <Fragment>
+      <Link to='/'> Back To Search </Link>
+      Hireable:{' '}
+      {hireable ? (
+        <i className='fa-solid fa-check' />
+      ) : (
+        <i className='fa-solid fa-circle-xmark ' />
+      )}
+      <div>
+        <div>
+          <img src={avatar_url} alt='profile image' />
+          <h1>{name}</h1>
+          <p>Location: {location}</p>
+        </div>
+        <div>
+          {bio && (
+            <Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+          )}
+          <a href={html_url}>Github Profile</a>
+          <ul>
+            <li>
+              <strong>Username: </strong> {login}
+            </li>
+            <li>
+              {company && (
+                <Fragment>
+                  <strong>Company: </strong> {company}
+                </Fragment>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <Fragment>
+                  <strong>Website: </strong> {blog}
+                </Fragment>
+              )}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div>Followers: {followers}</div>
+        <div>Following: {following}</div>
+        <div>Public Repos: {public_repos}</div>
+        <div>Public Gists: {public_gists}</div>
+      </div>
+    </Fragment>
+  );
+};
+
+User.propTypes = {
+  getUser: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 export default User;
